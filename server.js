1e5cfa99a3373ce462d03cb8cd992dd76e4f37af
Navya Pullagura <pullaguranavya2006@gmail.com>
2026-06-01 15:05:36 +0530

    update server.js

--- server.js
+++ server.js
@@ -7,22 +7,31 @@ dotenv.config();
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
+.then(() => {
     console.log("MongoDB Connected");
 })
-.catch(err=>{
-    console.log(err);
+.catch((err) => {
+    console.log("MongoDB Error:", err);
 });
 
+// Routes
 app.use("/api/leads", require("./routes/leadRoutes"));
 
+// DEFAULT ROUTE (IMPORTANT FOR RENDER TEST)
+app.get("/", (req, res) => {
+    res.send("MiniCRM Backend is Running 🚀");
+});
+
+// PORT FIX (VERY IMPORTANT FOR RENDER)
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
